758fce4d9d97258a7587e7edfee47e8dffeaea89
Megana <megana15081990@yandex.ru>
2024-11-29 00:25:18 +0700

    первый коммит

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,107 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { ProtectedRoute } from '../protected-route';
+
+// const App = () => (
+//   <div className={styles.app}>
+//     <AppHeader />
+//     <ConstructorPage />
+//   </div>
+// );
+
+// export default App;
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <Router>
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+        <Route
+          path='/feed/:number'
+          element={
+            <Modal>
+              <OrderInfo />
+            </Modal>
+          }
+        />
+        <Route
+          path='/ingredients/:id'
+          element={
+            <Modal>
+              <IngredientDetails />
+            </Modal>
+          }
+        />
+
+        <Route
+          path='/profile/orders/:number'
+          element={
+            <ProtectedRoute>
+              <Modal>
+                <OrderInfo />
+              </Modal>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
