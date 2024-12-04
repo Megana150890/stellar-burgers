0e11da75e1ce04e2cda8b26a6477c38371e0be55
Megana <megana15081990@yandex.ru>
2024-12-04 15:20:11 +0700

    сделано модальное окно с деталями ингредиента

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector } from 'react-redux';
+import { getIngredients } from '../../slise/ingredientsSlice';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
   /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams();
+  const ingredients = useSelector(getIngredients);
+
+  // const ingredientData = null;
+  const ingredientData = ingredients.find(
+    (ingredient) => ingredient._id === id
+  );
 
   if (!ingredientData) {
     return <Preloader />;
