0c528393d4015c9740f38ea0f35fb8139f69be5c
Megana <megana15081990@yandex.ru>
2024-12-09 20:29:33 +0700

    юзер работает

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,14 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { getFeedSelect } from '../../slise/feedSlice';
+import { orderSlice } from '../../slise/orderSlice';
 
 export const ProfileOrders: FC = () => {
   /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  // const orders: TOrder[] = [];
+  const orders: TOrder[] = useSelector(getFeedSelect);
 
   return <ProfileOrdersUI orders={orders} />;
 };
