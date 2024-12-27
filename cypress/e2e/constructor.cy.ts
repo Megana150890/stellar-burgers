5504c07e22f94abc4f01a79148b6b9a88b018b4b
Megana <megana15081990@yandex.ru>
2024-12-27 23:25:52 +0700

    начало тестов на cypress

---
+++ cypress/e2e/constructor.cy.ts
@@ -0,0 +1,62 @@
+const testBun = '643d69a5c3f7b9001cfa093d';
+const testSauce = '643d69a5c3f7b9001cfa0942';
+const testMainIngredient = '643d69a5c3f7b9001cfa0948';
+beforeEach(() => {
+  cy.window().then((win) => {
+    win.localStorage.setItem('refreshToken', 'mockRefreshToken');
+  });
+  cy.setCookie('accessToken', 'mockAccessToken');
+
+  cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
+    'ingredients'
+  );
+  cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' }).as('user');
+  cy.visit('http://localhost:4000'); 
+});
+
+afterEach(() => {
+  // Очищаем токены
+  cy.window().then((win) => {
+    win.localStorage.removeItem('refreshToken');
+  });
+  cy.clearCookie('accessToken');
+});
+
+describe('Burger constructor', () => {
+  it('отображение 15 ингредиентов', () => {
+    // Ждём завершения запроса на ингредиенты
+    cy.wait('@ingredients');
+  });
+
+  it('добавление ингредиетов', () => {
+    cy.get(`[data-cy=${testBun}]`).contains('Добавить').click();
+    cy.get('[data-cy=bun]').should('exist');
+
+    cy.get(`[data-cy=${testSauce}]`).contains('Добавить').click();
+    cy.get('[data-cy=mainIngredients]').should('exist');
+
+    cy.get(`[data-cy=${testMainIngredient}]`).contains('Добавить').click();
+    cy.get('[data-cy=mainIngredients]').should('exist');
+  });
+
+  describe('тест работы модальных окон', () => {
+    it('открытие модального окна - детали ингредиента', () => {
+      cy.get(`[data-cy=${testBun}]`).click();
+      cy.get('[data-cy =modal]').should('exist');
+    });
+
+    it('закрытие моадльного окна на крестик', () => {
+      cy.get(`[data-cy=${testBun}]`).click();
+      cy.get('[data-cy =modal]').should('exist');
+      cy.get('[data-cy=closeButton]').click();
+      cy.get('[data-cy =modal]').should('not.exist');
+    });
+
+    it('закрытие модального окна по клику на оверлей', () => {
+      cy.get(`[data-cy=${testBun}]`).click();
+      cy.get('[data-cy =modal]').should('exist');
+      cy.get('[data-cy=overlay]').click({ force: true }); //чтобы не перекрывался изображением ингредиента
+      cy.get('[data-cy =modal]').should('not.exist');
+    });
+  });
+});
