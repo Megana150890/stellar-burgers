4f92948ef48da3bded259112a511dc7093ae87fe
Megana <megana15081990@yandex.ru>
2024-12-28 20:06:59 +0700

    сделаны тесты на cypress

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -2,25 +2,25 @@ const testBun = '643d69a5c3f7b9001cfa093d';
 const testSauce = '643d69a5c3f7b9001cfa0942';
 const testMainIngredient = '643d69a5c3f7b9001cfa0948';
 beforeEach(() => {
-  cy.window().then((win) => {
-    win.localStorage.setItem('refreshToken', 'mockRefreshToken');
-  });
-  cy.setCookie('accessToken', 'mockAccessToken');
+  //   cy.window().then((win) => {
+  //     win.localStorage.setItem('refreshToken', 'mockRefreshToken');
+  //   });
+  //   cy.setCookie('accessToken', 'mockAccessToken');
 
   cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' }).as(
     'ingredients'
   );
-  cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' }).as('user');
-  cy.visit('http://localhost:4000'); 
+  //   cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' }).as('user');
+  cy.visit('http://localhost:4000');
 });
 
-afterEach(() => {
-  // Очищаем токены
-  cy.window().then((win) => {
-    win.localStorage.removeItem('refreshToken');
-  });
-  cy.clearCookie('accessToken');
-});
+// afterEach(() => {
+//   // Очищаем токены
+//   cy.window().then((win) => {
+//     win.localStorage.removeItem('refreshToken');
+//   });
+//   cy.clearCookie('accessToken');
+// });
 
 describe('Burger constructor', () => {
   it('отображение 15 ингредиентов', () => {
@@ -58,5 +58,41 @@ describe('Burger constructor', () => {
       cy.get('[data-cy=overlay]').click({ force: true }); //чтобы не перекрывался изображением ингредиента
       cy.get('[data-cy =modal]').should('not.exist');
     });
+  });
+});
+
+describe('тест на создание заказа', () => {
+  beforeEach(() => {
+    window.localStorage.setItem(
+      'refreshToken',
+      JSON.stringify('refreshToken-fake')
+    );
+    cy.setCookie('accessToken', 'mockAccessToken');
+
+    cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' }).as('user');
+    cy.intercept('POST', 'api/orders', { fixture: 'order.json' }).as('order');
+  });
+  afterEach(() => {
+    // Очищаем токены
+    cy.clearLocalStorage();
+    cy.clearCookies();
+  });
+  it('создание заказа', () => {
+    cy.get(`[data-cy=${testBun}]`).contains('Добавить').click();
+
+    cy.get(`[data-cy=${testSauce}]`).contains('Добавить').click();
+
+    cy.get(`[data-cy=${testMainIngredient}]`).contains('Добавить').click();
+
+    cy.get('[data-cy=button-order]')
+      .contains('Оформить заказ')
+      .click()
+      .intercept('POST', 'api/orders', { fixture: 'order.json' })
+      .as('order');
+    cy.get('[data-cy=modal]').contains('64480').should('exist');
+    cy.get('[data-cy=closeButton]').click();
+    cy.get('[data-cy =modal]').should('not.exist');
+    cy.get('[data-cy=bun]').should('have.length', 0);
+    cy.get('[data-cy=mainIngredients]').should('have.length', 0);
   });
 });
